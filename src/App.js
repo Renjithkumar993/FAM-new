af5f2b520ad84964d1f88d896ea7370f0a24a36e
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2024-07-08 17:36:16 -0300

    changed hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { Element } from 'react-scroll';
 import './App.css';
 import NavigationBar from './components/NavigationBar';
