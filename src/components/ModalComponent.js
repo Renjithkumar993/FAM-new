421317310d8ebef22d49548fc0fb159d5e2ed0db
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2024-07-19 13:11:20 -0300

    :fjdbfjd

--- src/components/ModalComponent.js
+++ src/components/ModalComponent.js
@@ -10,33 +10,41 @@ const ModalComponent = ({ open, handleClose, iframeSrc }) => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: '90%',
+        width: '100%',
         maxWidth: '800px',
-        height: '90%',
+        height: '100%',
         bgcolor: 'background.paper',
         boxShadow: 24,
-        p: 4,
         overflow: 'hidden',
         display: 'flex',
         flexDirection: 'column',
+        p: 0,
         '@media (max-width: 600px)': {
           width: '100%',
           height: '100%',
-          p: 2,
         },
       }}>
         <IconButton
-          sx={{ position: 'absolute', top: 8, right: 8 }}
+          sx={{ 
+            position: 'absolute', 
+            top: 8, 
+            left: 8, 
+            color: '#ff6341', // Orange color
+            zIndex: 1,
+            fontSize: '2rem' // Make the button larger
+          }}
           onClick={handleClose}
         >
-          <CloseIcon />
+          <CloseIcon fontSize="inherit" />
         </IconButton>
         <iframe
           src={iframeSrc}
           style={{
             width: '100%',
             height: '100%',
-            border: 'none'
+            border: 'none',
+            margin: 0,
+            padding: 0,
           }}
           title="Modal Content"
         ></iframe>
